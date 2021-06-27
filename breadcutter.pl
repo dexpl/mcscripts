615559f750841c70111c583f75ef38e2eeae6979
Vadim Raskhozhev <iamdexpl@gmail.com>
2021-06-27 21:33:38 +0300

    Squashed commit of the following:
    commit e0e3e1ab9523f9149207e61c0fc85fdf819247fa
    Author: Vadim Raskhozhev <iamdexpl@gmail.com>
    Date:   Sun Jun 27 21:28:02 2021 +0300
    
        new file:   breadcutter.pl
    
    commit bf2253eb627ba1eaced618adeb7e708905937a74
    Author: Vadim Raskhozhev <iamdexpl@gmail.com>
    Date:   Sun Jun 27 21:27:38 2021 +0300
    
        deleted:    ts.pl
    
    commit 93596cc46a15f32af5aa4647f81dcea342fac0bf
    Author: Vadim Raskhozhev <iamdexpl@gmail.com>
    Date:   Sun Jun 27 14:03:06 2021 +0300
    
        new file:   ts.pl

---
+++ breadcutter.pl
@@ -0,0 +1,72 @@
+#!/usr/bin/perl -l
+
+use strict;
+use warnings;
+
+my @timestamps;
+my %jobdescs;
+my $seconds_in_a_year = 60 * 60 * 24 * 365;
+my $vftemplate        = "select='%s', setpts=N/FRAME_RATE/TB";
+my $aftemplate        = "aselect='%s', asetpts=N/SR/TB";
+
+sub ts2seconds {
+    map {
+        my @hms = split /:/;
+        map { s/\D//gr } @hms;
+            $#hms > 2  ? warn "Strange timestamp: @hms"
+          : $#hms eq 2 ? $hms[0] * 60 * 60 + $hms[1] * 60 + $hms[2]
+          : $#hms eq 1 ? $hms[0] * 60 + $hms[1]
+          :              $hms[0];
+    } @_;
+}
+
+foreach my $jobdesc (@ARGV) {
+    open my $fh, '<', $jobdesc or die "Cannot read $jobdesc: $!";
+    my $mediafile;
+    while (<$fh>) {
+        chomp;
+        if ( $. == 1 ) {
+            unless (-r) {
+                warn "Cannot open $_, skipping $jobdesc";
+                last;
+            }
+            $mediafile = $_;
+        } else {
+            next if /^\s*(?:#|$)/;
+            my @seconds = ts2seconds split /\s+\D*\s*/;
+            if ( exists $jobdescs{$mediafile} ) {
+                push @{ $jobdescs{$mediafile} }, @seconds;
+            } else {
+                $jobdescs{$mediafile} = [@seconds];
+            }
+        }
+    }
+    close($fh);
+
+    foreach my $mfile ( keys %jobdescs ) {
+        my @cmdline;
+        for ( my $cnt = 0 ; $cnt < $#{ $jobdescs{$mfile} } ; $cnt += 2 ) {
+            push @cmdline, sprintf 'not(between(t\, %d\, %d))',
+              $jobdescs{$mfile}[$cnt],
+              $jobdescs{$mfile}[ $cnt + 1 ] // $seconds_in_a_year;
+        }
+        my $filter  = join '+', @cmdline;
+        my $vf      = sprintf $vftemplate, $filter;
+        my $af      = sprintf $aftemplate, $filter;
+        my @mfile   = split /\./, $mfile;
+        my $newname = pop @mfile;
+        $newname = join ".", ( @mfile, "out", $newname );
+        qx(ffmpeg -i $mfile -vf "$vf" -af "$af" $newname);
+    }
+}
+
+=pod
+Read a file consisting of a path to mediafile to be processed and timestamps of intervals to cut off.
+For example,
+
+movie.mpg
+0 10:55
+1:0:0 1:0:10
+1:30:0
+
+means "cut off movie.mpg from the beginning up to 10:55, then from 1:0:0 to 1:0:10, then from 1:30:0 to the end"
